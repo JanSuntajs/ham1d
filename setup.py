8f38a6b49ac6e2360c4f27778b5fee683a152ad9
Jan Suntajs <jansuntajs@gmail.com>
2019-09-10 13:56:51 +0200

    fixing setup.py basic script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(name='ham1d',
+      version='1.0.0',
+      description='A module for calculations with 1D quantum hamiltonians',
+      url='https://github.com/JanSuntajs/ham1d',
+      author='Jan Suntajs',
+      author_email='Jan.Suntajs@ijs.si',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[],
+      dependency_links=[],
+      zip_safe=False)
