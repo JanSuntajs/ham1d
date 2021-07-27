af3ffe3878dd3fbd975ae839a2e7549797268735
Jan Suntajs <jansuntajs@gmail.com>
2021-07-27 12:01:54 +0200

    omitting dependency links, using install_requires pep 508 requirement urls instead

--- setup.py
+++ setup.py
@@ -9,9 +9,11 @@ setup(name='ham1d',
       author_email='Jan.Suntajs@ijs.si',
       license='MIT',
       packages=find_packages(),
-      install_requires=['spectral_stats'],
-      dependency_links=[('https://github.com/JanSuntajs/'
-                         'spectral_statistics_tools/tarball/'
-                         'master/#egg=spectral_stats-1.1.1'),
-                       ],
+      install_requires=[('spectral_stats @ git+https://github.com/JanSuntajs/'
+                         'spectral_statistics_tools/tarball/master/'
+                         '#egg=spectral_stats-1.1.1'), ],
+      # dependency_links=[('https://github.com/JanSuntajs/'
+      #                   'spectral_statistics_tools/tarball/'
+      #                   'master/#egg=spectral_stats-1.1.1'),
+      #                 ],
       zip_safe=False)
